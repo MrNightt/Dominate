91b5f3df3e1e824b5edcdfc7acdd10cb65fbe760
MrNightt <Editsbycosta@gmail.com>
2017-05-09 20:53:52 +0100

    Add the visualization of the possible moves.
    Minor code clean.

--- Dominate/src/pt/europeia/dominate/controllers/GameController.java
+++ Dominate/src/pt/europeia/dominate/controllers/GameController.java
@@ -1,9 +1,9 @@
 package pt.europeia.dominate.controllers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Orientation;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -27,7 +27,7 @@ public class GameController {
 
 	@FXML
 	Text p1, p2;
-	
+
 	@FXML
 	Text pturn;
 
@@ -40,12 +40,8 @@ public class GameController {
 
 		game = new Dominate();
 
-		for(int i = 0; i < canvas.getWidth(); i += canvas.getWidth()/8) {
-			gc.setStroke(Color.BLACK);
-			gc.strokeLine(0, i, canvas.getWidth(), i);
-			gc.strokeLine(i, 0, i, canvas.getWidth());
-		}
-
+		drawBG();
+		
 		p1.setText(game.getP1Score()+"");
 		p2.setText(game.getP2Score()+"");
 		pturn.setText(game.getTurn()+"");
@@ -56,9 +52,25 @@ public class GameController {
 	}
 
 	/**
+	 * Draws the BackGround
+	 */
+	public void drawBG() {
+		gc.setFill(Color.WHITESMOKE);
+		gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		for(int i = 0; i < canvas.getWidth(); i += canvas.getWidth()/8) {
+			gc.setStroke(Color.BLACK);
+			gc.strokeLine(0, i, canvas.getWidth(), i);
+			gc.strokeLine(i, 0, i, canvas.getWidth());
+		}
+	}
+	
+	/**
 	 * Updates the canvas
 	 */
 	public void update() {
+		
+		drawBG();
+		
 		for(int i = 0; i < game.getTable().length; i++) {
 
 			for(int j = 0; j < game.getTable().length; j++) {
@@ -74,17 +86,31 @@ public class GameController {
 
 			}
 		}
-		
-		/*for(int i = 0; i < game.getTablePlays().size(); i++) {
-			gc.fillOval(game.getTablePlays().get(i), game.getTablePlays().get(i), square/3, square/3);
-		}*/
+
+		gc.setFill(Color.GREEN);
+		for(int i = 0; i < game.getTablePlays().size(); i++) {
+			ArrayList<int[]> x = game.getTablePlays();
+			double middle = square/2 - square/6;
+			gc.fillOval(x.get(i)[0]*square + middle, x.get(i)[1]*square + middle, square/3, square/3);
+		}
 
 		p1.setText(game.getP1Score()+"");
 		p2.setText(game.getP2Score()+"");
-		
+
 		System.out.println(game.getTurn());
 		for (Object each : game.getTablePlays()) {
 			System.out.println(Arrays.toString((int[])each));
+		}
+
+		//End of the game
+		if((game.getP1Score() + game.getP2Score()) == 64) {
+			Group bp = new Group();
+			Text winner = new Text(20,30, game.getWinner() + "'s " + "WON");
+			bp.getChildren().add(winner);
+			Scene scene = new Scene(bp,100,50);
+			Stage stage = new Stage();
+			stage.setScene(scene);
+			stage.show();
 		}
 
 	}
@@ -106,25 +132,14 @@ public class GameController {
 				break;
 			}		
 		}
-		
+
 		System.out.println("cell: " + x + " ; " + y);
-		
+
 		game.move(x, y);
 		pturn.setText(""+game.getTurn());
 		update();
-		
-		//End of the game
-		if((game.getP1Score() + game.getP2Score()) == 64) {
-			Group bp = new Group();
-			Text winner = new Text(20,30, game.getWinner() + "'s " + "WON");
-			bp.getChildren().add(winner);
-			Scene scene = new Scene(bp,100,50);
-			Stage stage = new Stage();
-			stage.setScene(scene);
-			stage.show();
-		}
-			
+
 	}
-	
-	
+
+
 }
